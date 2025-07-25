d577a2c658728e1d3525d3495ecd958b41883ec6
Nathan Kimborowicz <njkimbo99@hotmail.com>
2025-07-24 23:05:37 -0400

    Seperated data out into another file

---
+++ f1_schedule_data.py
@@ -0,0 +1,122 @@
+schedule_2025 = {
+        "race1": {
+            "name": "Australian GP",
+            "date": "3/16/2025",
+            "time": "12am"
+        },
+        "race2": {
+            "name": "Chinese GP",
+            "date": "3/23/2025",
+            "time": "3am"
+        },
+        "race3": {
+            "name": "Japanese GP",
+            "date": "4/6/2025",
+            "time": "1am"
+        },
+        "race4": {
+            "name": "Bharain GP",
+            "date": "4/13/2025",
+            "time": "11am"
+        },
+        "race5": {
+            "name": "Saudi Arabian GP",
+            "date": "4/20/2025",
+            "time": "1pm"
+        },
+        "race6": {
+            "name": "Miami GP",
+            "date": "5/4/2025",
+            "time": "4pm"
+        },
+        "race7": {
+            "name": "Emilia Romagna (Imola) GP",
+            "date": "5/18/2025",
+            "time": "9am"
+        },
+        "race8": {
+            "name": "Monaco GP",
+            "date": "5/25/2025",
+            "time": "9am"
+        },
+        "race9": {
+            "name": "Spanish GP",
+            "date": "6/1/2025",
+            "time": "9am"
+        },
+        "race10": {
+            "name": "Canadian GP",
+            "date": "6/15/2025",
+            "time": "2pm"
+        },
+        "race11": {
+            "name": "Austrian GP",
+            "date": "6/29/2025",
+            "time": "9am"
+        },
+        "race12": {
+            "name": "British GP",
+            "date": "7/6/2025",
+            "time": "10am"
+        },
+        "race13": {
+            "name": "Belgian GP",
+            "date": "7/27/2025",
+            "time": "9am"
+        },
+        "race14": {
+            "name": "Hungarian GP",
+            "date": "8/3/2025",
+            "time": "9am"
+        },
+        "race15": {
+            "name": "Dutch GP",
+            "date": "8/31/2025",
+            "time": "9am"
+        },
+        "race16": {
+            "name": "Italian GP",
+            "date": "9/7/2025",
+            "time": "9am"
+        },
+        "race17": {
+            "name": "Azerbaijan GP",
+            "date": "9/21/2025",
+            "time": "7am"
+        },
+        "race18": {
+            "name": "Singapore GP",
+            "date": "8/5/2025",
+            "time": "8am"
+        },
+        "race19": {
+            "name": "United States GP",
+            "date": "10/19/2025",
+            "time": "3pm"
+        },
+        "race20": {
+            "name": "Mexican GP",
+            "date": "10/26/2025",
+            "time": "4pm"
+        },
+        "race21": {
+            "name": "Brazilian GP",
+            "date": "11/9/2025",
+            "time": "12pm"
+        },
+        "race22": {
+            "name": "Las Vegas GP",
+            "date": "11/22/2025",
+            "time": "11pm"
+        },
+        "race23": {
+            "name": "Qatar GP",
+            "date": "11/30/2025",
+            "time": "11am"
+        },
+        "race24": {
+            "name": "Abu Dhabi GP",
+            "date": "12/7/2025",
+            "time": "8am"
+        }
+    }

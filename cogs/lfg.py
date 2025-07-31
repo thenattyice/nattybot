a45ed6c8b778c37a6d7f7f3c555aefca9cf1ad9b
Nathan Kimborowicz <njkimbo99@hotmail.com>
2025-07-31 15:19:04 -0400

    Added lfg commands for Rematch and MTG, allowed for role mentions in their responses

--- cogs/lfg.py
+++ cogs/lfg.py
@@ -11,7 +11,10 @@ class LookingForGroup(commands.Cog):
         
         # Registrations of commands
         self.bot.tree.add_command(self.rl, guild=self.guild_object)
+        self.bot.tree.add_command(self.rematch, guild=self.guild_object)
+        self.bot.tree.add_command(self.mtg, guild=self.guild_object)
         
+    # Rocket league
     @app_commands.command(name="rl", description="Tag all user's with the Rocket League role to get some games going!")
     async def rl(self, interaction: discord.Interaction):
         role_id = self.bot.game_roles["rocket league"]
@@ -21,4 +24,28 @@ class LookingForGroup(commands.Cog):
             await interaction.response.send_message("Role not found!", ephemeral=True)
             return
         
-        await interaction.response.send_message(f"{role.mention}")
+        await interaction.response.send_message(f"{role.mention} Time for some games!",allowed_mentions=discord.AllowedMentions(roles=True))
+        
+    # Rematch
+    @app_commands.command(name="rematch", description="Tag all user's with the Rematch role to get some games going!")
+    async def rematch(self, interaction: discord.Interaction):
+        role_id = self.bot.game_roles["rematch"]
+        role = interaction.guild.get_role(role_id)
+        
+        if role is None:
+            await interaction.response.send_message("Role not found!", ephemeral=True)
+            return
+        
+        await interaction.response.send_message(f"{role.mention} Time for some matches!",allowed_mentions=discord.AllowedMentions(roles=True))
+        
+    # MTG
+    @app_commands.command(name="mtg", description="Tag all user's with the Magic Nerd role to get some games going!")
+    async def mtg(self, interaction: discord.Interaction):
+        role_id = self.bot.game_roles["mtg"]
+        role = interaction.guild.get_role(role_id)
+        
+        if role is None:
+            await interaction.response.send_message("Role not found!", ephemeral=True)
+            return
+        
+        await interaction.response.send_message(f"{role.mention} Time for some commander!",allowed_mentions=discord.AllowedMentions(roles=True))

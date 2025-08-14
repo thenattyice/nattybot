0bf1747697001367dd41505e42ec2fd5d92fdeab
Nathan Kimborowicz <njkimbo99@hotmail.com>
2025-08-14 15:02:21 -0400

    Final cleanup on cog loading

--- cogs/shop/businesses.py
+++ cogs/shop/businesses.py
@@ -63,5 +63,4 @@ class Businesses(commands.Cog):
 async def setup(bot):
     cog = Businesses(bot)          
     await bot.add_cog(cog)         
-    cog.daily_payout.start()  
-    print("Businesses cog loaded successfully!")
+    cog.daily_payout.start()
